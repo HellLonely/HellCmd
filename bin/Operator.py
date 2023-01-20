14e7dfa976f684b7df58de03d8c39e34b6c530fd
HellLonely <helllonely404@gmail.com>
2023-01-20 13:31:33 +0100

    Resolución llamada a comando cd

--- bin/Operator.py
+++ bin/Operator.py
@@ -1,5 +1,7 @@
 import os
 import bin.Help as Help
+import bin.Directory as Dir
+
 
 def connect(command):
     command = str(command)
@@ -7,5 +9,14 @@ def connect(command):
         Help.helpmenu()
     elif command == 'cls' or command =='clear':
         os.system ("cls") 
+    elif command == 'exit' or command =='close':
+        exit()
+    elif command == 'dir' or command =='ls':
+        Dir.DirSimple()
+    elif command.startswith('cd'):
+        path = command.split(' ')[1]
+        Dir.cd(path)
 
+    
+    
 
